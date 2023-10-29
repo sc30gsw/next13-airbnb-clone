c499fa43f4fe556962c1db186c4c6ea713f3384d
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-30 06:24:37 +0900

    💄  ナビゲーションバーの作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 
+import Navbar from '@/components/navbar/Navbar'
+
 const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
