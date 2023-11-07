cde68ef4a5ea9d77de382b586cbfe7cf6f416148
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-08 07:48:01 +0900

    ✨ プロバイダー認証機能を実装

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -103,13 +103,13 @@ const RegisterModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn('google', { callbackUrl: '/' })}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn('github', { callbackUrl: '/' })}
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row justify-center items-center gap-2">
