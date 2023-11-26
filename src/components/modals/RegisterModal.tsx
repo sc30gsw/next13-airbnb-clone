30eb71cb261ccc0e3f6d634d0d37f1a9507ed134
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-26 09:48:33 +0900

    ✨ ユーザー新規登録をServerActionsで行うよう修正

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
 import { signIn } from 'next-auth/react'
 import React, { useCallback, useState } from 'react'
 import type { SubmitHandler } from 'react-hook-form'
@@ -9,6 +10,7 @@ import toast from 'react-hot-toast'
 import { AiFillGithub } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 
+import registerUser from '@/app/actions/registerUser'
 import Button from '@/components/Button'
 import Heading from '@/components/Heading'
 import Input from '@/components/inputs/Input'
@@ -20,6 +22,7 @@ import { registerSchema } from '@/types/RegisterForm'
 const RegisterModal = () => {
   const registerModal = useRegisterModal()
   const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
 
   const { handleSubmit, control, reset, setError } = useForm<RegisterForm>({
     resolver: zodResolver(registerSchema),
@@ -34,16 +37,12 @@ const RegisterModal = () => {
     async (data) => {
       setIsLoading(true)
       try {
-        const response = await fetch('/api/auth/register', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(data),
-        })
+        const response = await registerUser(data)
 
-        if (!response.ok) {
-          const error = await response.json()
-          if (error.message === 'Email is taken')
-            setError('email', { type: 'manual', message: error.message })
+        if (response.error) {
+          response.error === 'Email is taken'
+            ? setError('email', { type: 'manual', message: response.error })
+            : toast('Something Went Wrong')
 
           return
         }
@@ -59,13 +58,14 @@ const RegisterModal = () => {
 
         registerModal.onClose()
         reset()
+        router.refresh()
       } catch (err) {
         toast.error('Something Went Wrong.')
       } finally {
         setIsLoading(false)
       }
     },
-    [registerModal, setError, reset],
+    [registerModal, setError, reset, router],
   )
 
   const bodyContent = (
