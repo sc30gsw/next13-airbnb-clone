c3879fc5a8c877cd607a6ae1c176424e844b5996
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-26 16:08:14 +0900

    🚸 最大値のバリデーションを追加

--- src/types/LoginForm.ts
+++ src/types/LoginForm.ts
@@ -1,10 +1,15 @@
 import z from 'zod'
 
 export const loginSchema = z.object({
-  email: z.string().min(1, 'required').email('format is incorrect'),
+  email: z
+    .string()
+    .min(1, 'required')
+    .max(128, 'please enter at max 128 characters')
+    .email('format is incorrect'),
   password: z
     .string()
     .min(8, 'please enter at least 8 characters')
+    .max(128, 'please enter at max 128 characters')
     .refine(
       (password: string) => /[A-Za-z]/.test(password) && /[0-9]/.test(password),
       'password must contain both letters and numbers',
--- src/types/RegisterForm.ts
+++ src/types/RegisterForm.ts
@@ -1,11 +1,19 @@
 import z from 'zod'
 
 export const registerSchema = z.object({
-  name: z.string().min(8, 'please enter at latest 8 characters'),
-  email: z.string().min(1, 'required').email('format is incorrect'),
+  name: z
+    .string()
+    .min(8, 'please enter at latest 8 characters')
+    .max(128, 'please enter at max 128 characters'),
+  email: z
+    .string()
+    .min(1, 'required')
+    .max(128, 'please enter at max 128 characters')
+    .email('format is incorrect'),
   password: z
     .string()
     .min(8, 'please enter at least 8 characters')
+    .max(128, 'please enter at max 128 characters')
     .refine(
       (password: string) => /[A-Za-z]/.test(password) && /[0-9]/.test(password),
       'password must contain both letters and numbers',
