a76b8c113fcfa46e309f0d949d8f6c08f0d32288
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-06 15:41:37 +0900

    🚚 ファイル名修正、paramsをidとする

--- src/hooks/useOnDelete.ts
+++ src/hooks/useOnDelete.ts
@@ -1,4 +1,4 @@
-import type { Reservation } from '@prisma/client'
+import type { Listing, Reservation } from '@prisma/client'
 import { useRouter } from 'next/navigation'
 import { useCallback, useState } from 'react'
 import toast from 'react-hot-toast'
@@ -6,23 +6,26 @@ import toast from 'react-hot-toast'
 import type { SafeUser } from '@/types/SafeUser'
 
 type TParams = {
-  params: { reservationId: string; userId: string }
+  // reservationIdとlistingIdが渡ってくるためidと命名
+  params: { id: string; userId: string }
 }
 
-const useOnCancel = (
-  deleteModel: ({ params }: TParams) => Promise<{ reservation: Reservation }>,
+const useOnDelete = (
+  deleteModel: ({
+    params,
+  }: TParams) => Promise<{ reservation?: Reservation; listing?: Listing }>,
   currentUser?: SafeUser | null,
 ) => {
   const router = useRouter()
   const [deletingId, setDeletingId] = useState('')
 
-  const onCancel = useCallback(
+  const onDelete = useCallback(
     async (id: string) => {
       setDeletingId(id)
 
       try {
         await deleteModel({
-          params: { reservationId: id, userId: currentUser?.id as string },
+          params: { id, userId: currentUser?.id as string },
         })
         toast.success('Reservation cancelled')
         router.refresh()
@@ -35,7 +38,7 @@ const useOnCancel = (
     [currentUser?.id, deleteModel, router],
   )
 
-  return { deletingId, onCancel }
+  return { deletingId, onDelete }
 }
 
-export default useOnCancel
+export default useOnDelete
